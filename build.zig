04c68f8d529277b0a082ab1cc6ecdf4eee890f68
Simon Hartcher <simon@simonhartcher.com>
2025-04-26 17:38:48 +1000

    chore: update for Zig 0.14

--- build.zig
+++ build.zig
@@ -4,15 +4,7 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
@@ -22,7 +14,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const tree_sitter_lib = b.dependency("tree-sitter", .{
+    const tree_sitter_lib = b.dependency("tree_sitter", .{
         .target = target,
         .optimize = optimize,
     });
@@ -36,63 +28,46 @@ pub fn build(b: *std.Build) void {
     exe.linkLibrary(tree_sitter_lib.artifact("tree-sitter"));
     exe.root_module.addImport("tree-sitter", tree_sitter_module);
 
-    const tree_sitter_gdscript = b.addStaticLibrary(.{
+    const dep_tree_sitter_gdscript = b.dependency("tree_sitter_gdscript", .{});
+
+    const lib_tree_sitter_gdscript = b.addStaticLibrary(.{
         .name = "tree-sitter-gdscript",
         .target = target,
         .optimize = optimize,
     });
 
-    tree_sitter_gdscript.addCSourceFiles(.{
+    lib_tree_sitter_gdscript.addCSourceFiles(.{
         .files = &[_][]const u8{
-            "src/parser.c",
-            "src/scanner.c",
+            "parser.c",
+            "scanner.c",
         },
-        .root = b.path("lib/tree-sitter-gdscript/"),
+        .root = dep_tree_sitter_gdscript.builder.path("src"),
     });
-    tree_sitter_gdscript.linkLibC();
-    tree_sitter_gdscript.addIncludePath(tree_sitter_lib.path("lib/include"));
-    tree_sitter_gdscript.addIncludePath(b.path("lib/tree-sitter-gdscript/src"));
+    lib_tree_sitter_gdscript.linkLibC();
+    lib_tree_sitter_gdscript.addIncludePath(tree_sitter_lib.path("lib/include"));
+    lib_tree_sitter_gdscript.addIncludePath(dep_tree_sitter_gdscript.builder.path("src"));
 
     const tree_sitter_gdscript_module = b.createModule(.{
         .root_source_file = b.path("lib/lib-tree-sitter-gdscript.zig"),
     });
-    tree_sitter_gdscript_module.addIncludePath(b.path("lib/tree-sitter-gdscript/bindings/swift/"));
+    tree_sitter_gdscript_module.addIncludePath(dep_tree_sitter_gdscript.builder.path("bindings/swift/"));
 
-    exe.linkLibrary(tree_sitter_gdscript);
+    exe.linkLibrary(lib_tree_sitter_gdscript);
     exe.root_module.addImport("tree-sitter-gdscript", tree_sitter_gdscript_module);
 
-    b.installArtifact(tree_sitter_gdscript);
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    b.installArtifact(lib_tree_sitter_gdscript);
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -109,9 +84,6 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
