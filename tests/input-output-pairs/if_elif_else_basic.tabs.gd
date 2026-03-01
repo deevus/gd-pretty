1bc5b550a9d1ce9e2f148992f50303ed60441ace
Simon Hartcher <simon@simonhartcher.com>
2026-03-01 07:32:30 +0000

    test: add dedicated if/elif/else basic test with desired output
    Covers basic if/elif/else formatting, operator spacing in conditions,
    multiple elif chains, inline comments on clauses, and nested if statements.

---
+++ tests/input-output-pairs/if_elif_else_basic.tabs.gd
@@ -0,0 +1,37 @@
+func foo():
+	if true:
+		pass
+	if x == 1:
+		var a = 1
+	elif x == 2:
+		var b = 2
+	else:
+		var c = 3
+	if x > 0 and y < 10:
+		return x
+	elif x == 0:
+		return 0
+	elif x < 0:
+		return -1
+	else:
+		return -2
+
+func bar():
+	if condition: # inline comment
+		pass
+	elif other: # another comment
+		pass
+	else: # final comment
+		pass
+
+func nested():
+	if a:
+		if b:
+			pass
+		else:
+			pass
+	elif c:
+		if d:
+			pass
+	else:
+		pass
